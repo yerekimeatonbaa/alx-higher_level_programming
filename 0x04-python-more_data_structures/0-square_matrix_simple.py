f93ba02201f6b0138ce4981cf3f7be98b1eabac8
Tonbra Yerekimea <yerekimeatonbaa@gmail.com>
2022-12-07 19:40:17 -0800

    first commit o fthe project

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+
+def square_matrix_simple(matrix=[]):
+
+    return [[y ** 2 for y in x] for x in matrix]
---
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+def simple_delete(a_dictionary, key=""):
+
+    a_dictionary.pop(key, None)
+
+    return a_dictionary
