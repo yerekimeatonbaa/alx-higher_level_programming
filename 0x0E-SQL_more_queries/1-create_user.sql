cbdb32a4d9d3b1e6aa37841814ba886ffe653b17
Tonbra Yerekimea <yerekimeatonbaa@gmail.com>
2023-02-16 12:56:14 -0800

    corrections

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,6 +1,6 @@
 -- write a script that creates MySQL server user user_0d_1
 -- The password should be set to user_0d_1_pwd
 
-CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL PRIVILEGES ON * . * TO 'user_0d_1'@'localhost';
+CREATE USER IF NOT EXISTS user_0d_1 @ localhost IDENTIFIED BY user_0d_1_pwd;
+GRANT ALL PRIVILEGES ON * . * TO user_0d_1 @ localhost;
 FLUSH PRIVILEGES;
