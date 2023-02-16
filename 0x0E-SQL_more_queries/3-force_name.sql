896d351bb85c4290764e90aa9662c078e6436826
Tonbra Yerekimea <yerekimeatonbaa@gmail.com>
2023-02-16 13:59:48 -0800

    fh

--- 0x0E-SQL_more_queries/3-force_name.sql
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -1,8 +1,8 @@
-# creates the table force_name on your MySQL server.
-# force_name description:
-# id INT
-# name VARCHAR(256) can’t be null
-# The database name will be passed as an argument of the mysql command
-# If the table force_name already exists, script should not fail
+-- creates the table force_name on your MySQL server.
+-- force_name description:
+-- id INT
+-- name VARCHAR(256) can’t be null
+-- The database name will be passed as an argument of the mysql command
+-- If the table force_name already exists, script should not fail
 
 CREATE TABLE IF NOT EXISTS force_name (id INT, name VARCHAR(256) NOT NULL);
