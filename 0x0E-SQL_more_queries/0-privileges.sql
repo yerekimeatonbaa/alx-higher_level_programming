ae3e9aa00183d01dcf3c47553e16016b8e413fb3
Tonbra Yerekimea <yerekimeatonbaa@gmail.com>
2023-02-15 18:23:39 -0800

    testing the first script

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,5 +1,4 @@
-# A script that tlist all the privileges of user_0d_1 & user_0d_2 on the local host
+-- lists all privileges of the MySQL users user_0d_1 and user_0d_2
 
-SHOW GRANTS FOR user_0d_1@localhost;
-SHOW GRANTS FOR user_0d_2@localhost;
-
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
