1575ea8fbddeee7f5f6253f48341450e3c994918
Tonbra Yerekimea <yerekimeatonbaa@gmail.com>
2023-02-16 20:09:42 -0800

    all commits of the project

---
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -0,0 +1,4 @@
+-- lists all records of the table second_table from the database hbtn_0c_0
+
+SELECT score, name FROM second_table WHERE name IS NOT NULL
+ORDER BY score DESC;
---
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -0,0 +1,12 @@
+-- creates a table called first_table in your MySQL server.
+
+CREATE TABLE IF NOT EXISTS second_table (id INT,
+       name VARCHAR(256),
+       score INT);
+
+-- initialize the table	with 4 ids.
+INSERT INTO second_table (id, name, score) VALUES
+       (1, "John", 10),
+       (2, "Alex", 3),
+       (3, "Bob", 14),
+       (4, "George", 8);
