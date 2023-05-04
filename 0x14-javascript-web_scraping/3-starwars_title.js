842b7ad4a26864358f5361194a33716c76a64811
yerekimeatonbaa <yerekimeatonbaa@gmail.com>
2023-05-04 03:58:55 +0200

    Delete 0x14-javascript-web_scraping directory

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/node
-const request = require('request');
-const movieId = process.argv[2];
-const route = 'https://swapi-api.hbtn.io/api/films/' + movieId;
-request(route, (err, res, body) => {
-  if (err) throw err;
-  // Display the retrieved content
-  console.log(JSON.parse(body).title);
-});
