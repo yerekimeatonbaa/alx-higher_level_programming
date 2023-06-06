3b67b428225df12c5945b28317f0578513e0f234
root <yerekimeatonbaa@gmail.com>
2023-06-05 19:19:08 -0700

    hg

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-class Rectangle():
-    pass
+Rectangle = __import__('0-rectangle').Rectangle
 my_rectangle = Rectangle()
 print(type(my_rectangle))
 print (my_rectangle.__dict__)
+
