5a480393e326c61bbbfdc78759f3f504373dc0a9
root <yerekimeatonbaa@gmail.com>
2023-06-13 18:46:03 -0700

    corrections

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,8 +1,15 @@
 #!/usr/bin/python3
 
 
-def read_file(filename=""):
-    with open(filename, "r", encoding="UTF-8") as f:
-        for line in file:
-            print(line, end="")
 
+"""
+0-read_file.py
+a function that reads a text file (UTF8) and prints it to stdout
+"""
+
+
+def read_file(filename=""):
+    """read file utf8 con with """
+    with open(filename, encoding='UTF-8') as f:
+        for line in f:
+            print(line, end='')
