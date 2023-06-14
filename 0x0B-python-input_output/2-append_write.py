02e93e8b3656081c1f6c58a064b130f36c8848a2
root <yerekimeatonbaa@gmail.com>
2023-06-13 19:04:02 -0700

    corrections

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -2,9 +2,16 @@
 """ Module that contains a function that appends to a text file
 """
 
-def write_file(filename="", text=""):
-    pass
-with open(filename, 'a', encoding="utf-8") as f:
+def append_write(filename="", text=""):
+    """ Function that appends to a text file
+    Args:
+        filename: filename
+        text: text to write
+    Raises
+        Exception: when the file can be opened
+    """
+
+    with open(filename, 'a', encoding="utf-8") as f:
         return f.write(text)
 
 
