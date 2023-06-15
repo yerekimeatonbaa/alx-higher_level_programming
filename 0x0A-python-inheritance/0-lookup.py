726df08991a7baa0f107751c32a1db3997a73f44
root <yerekimeatonbaa@gmail.com>
2023-06-14 21:14:34 -0700

    corrections

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 
+"""Defines an object attribute lookup function."""
+
 
 def lookup(obj):
-    """returns all objects in an objects dictionary
-        -> as a list
-    """
-    return dir(obj)
+    """Return a list of an object's available attributes."""
+    return (dir(obj))
 
 
--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,14 +1,12 @@
 #!/usr/bin/python3
 
-"""My list class Module"""
+"""Defines an inherited list class MyList."""
+
 
 class MyList(list):
-    """Class with method print_sorted"""
-    pass
+    """Implements sorted printing for the built-in list class."""
 
     def print_sorted(self):
-         """sorted list Method"""
-
-        print(sorted(list(self)))
-
+        """Print a list in sorted ascending order."""
+        print(sorted(self))
 
