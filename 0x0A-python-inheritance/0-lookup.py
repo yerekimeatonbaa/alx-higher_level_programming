1655b62291024d4d90028336566ad88ea07b4038
root <yerekimeatonbaa@gmail.com>
2023-06-14 19:35:07 -0700

    first commit

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+#Module with the method lookup
+
+def lookup(obj):
+    pass
+#returns all objects in an objects dictionary
+
+
+    return dir(obj)
+
---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+#My list class Module
+
+class MyList(list):
+    pass
+
+    def print_sorted(self):
+        #Methot that sorted a list
+
+        print(sorted(list(self)))
+
+
