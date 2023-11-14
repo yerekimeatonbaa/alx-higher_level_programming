e6f7bd784e1899ce6c1386f6c8eae4be5d79b3b0
root <yerekimeatonbaa@outlook.com>
2023-11-14 07:50:53 -0800

    first commit

--- 0x12-javascript-warm_up/0-javascript_is_amazing.js
+++ 0x12-javascript-warm_up/0-javascript_is_amazing.js
@@ -1,3 +1,6 @@
+#!/usr/bin/node
+
+
 // create a constant variable called myVar with the value "JavaScript is amazing"
 const myVar = "JavaScript is amazing";
 // use console.log(...) to print all output
