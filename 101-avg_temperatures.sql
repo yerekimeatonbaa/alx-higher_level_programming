feb6ef72f6b965af71c997138e7e634e315fe2a8
root <yerekimeatonbaa@gmail.com>
2023-11-30 20:29:02 -0800

    new commit

--- 101-avg_temperatures.sql
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
--- script that displays the average temperature (Fahrenheit) by city ordered by
--- temperature desending order
-SELECT `city`, AVG(`value`) AS `avg_temp`
-FROM `temperatures`
-GROUP BY `city`
-ORDER BY `avg_temp` DESC;
