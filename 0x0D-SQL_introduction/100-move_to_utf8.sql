3bcf5392f47d135b914ca6acd1bf1d11849ce88b
root <yerekimeatonbaa@gmail.com>
2024-02-16 22:36:51 -0800

    all files commited

---
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+-- converts hbtn_0c_0 database to UTF8
+ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+USE hbtn_0c_0;
+-- convert first_table to UTF8
+ALTER TABLE first_table
+CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
