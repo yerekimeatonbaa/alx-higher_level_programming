d5f5410d65df5ef9d5e35a462c47a3cc6c8eebb9
root <yerekimeatonbaa@gmail.com>
2024-02-18 15:40:43 -0800

    errors corrected

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -3,5 +3,5 @@
 -- password should be set to user_0d_1_pwd
 
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL PIVILEGES ON *.* TO 'user_0d_1'@'localhost';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
 FLUSH PRIVILEGES;
--- 0x0E-SQL_more_queries/3-force_name.sql
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -1,6 +1,6 @@
--- write a script that creates a force_nameon mysql server\
--- id INT, name VARCHAR(256) can't be null
--- The database name will be passed as an argument of the mysql command
--- If the table force_name already exists, your script should not fail
+-- Creates the table force_name.
+CREATE TABLE IF NOT EXISTS `force_name` (
+    `id`   INT,
+    `name` VARCHAR(256) NOT NULL
+);
 
-CREATE TABLE IF NOT EXISTS 'force_name' ('id' INT, 'name' VARCHAR(256) NOT NULL);
