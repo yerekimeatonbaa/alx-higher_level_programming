f61b7a312379bfe810857f88c97487af04aaf6b1
root <yerekimeatonbaa@gmail.com>
2024-03-17 17:05:12 -0700

    1st commit

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,16 +1,6 @@
 #!/usr/bin/node
-class Rectangle {
-    constructor(w, h) {
-        if (w <= 0 || h <= 0 || !Number.isInteger(w) || !Number.isInteger(h)) {
-            // If w or h is equal to 0 or not a positive integer, create an empty object
-            return {};
-        } else {
-            // Initialize the instance attribute width with the value of w
-            this.width = w;
-            // Initialize the instance attribute height with the value of h
-            this.height = h;
-        }
-    }
-}
-
-module.exports = Rectangle;
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if (w >= 0 && h >= 0) { [this.width, this.height] = [w, h]; }
+  }
+};
