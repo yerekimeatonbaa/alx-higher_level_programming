345040093c012937088870b2760ce45362dc8f44
root <yerekimeatonbaa@gmail.com>
2024-03-27 21:02:46 -0700

    1st commit

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++
@@ -1,8 +0,0 @@
--- Write a script that creates database hbtn_0d_2 & the user user_0d_2
--- User should have only SELECT privilege in the database
--- User password set to user_0d_2_pwd
-
-CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
-CREATE USER IF NOT EXISTS user_0d_2@localhost IDENTIFIED BY 'user_0d_2_pwd';
-GRANT SELECT ON hbtn_0d_2.* TO user_0d_2@localhost;
-FLUSH PRIVILEGES;
--- 0x13-javascript_objects_scopes_closures/100-data.js
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/node
-const list = [1, 2, 3, 4, 5];
-module.exports = list;
