9410bd4284343f9097aa5ce8e2c42873b6461bb4
root <yerekimeatonbaa@gmail.com>
2024-04-02 21:44:02 -0700

    1st commit

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,12 +1,16 @@
-import urllib.request
+#!/usr/bin/python3
+"""A script that
+- fetches https://alx-intranet.hbtn.io/status.
+- uses urlib package
+"""
 
-url = 'https://alx-intranet.hbtn.io/status'
 
-with urllib.request.urlopen(url) as response:
-    body = response.read()
-    utf8_content = body.decode('utf-8')
+if __name__ == '__main__':
+    import urllib.request
 
-print("Body response:")
-print(f"\t- type: {type(body)}")
-print(f"\t- content: {body}")
-print(f"\t- utf8 content: {utf8_content}")
+    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as res:
+        content = res.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(content)))
+        print("\t- content: {}".format(content))
+        print("\t- utf8 content: {}".format(content.decode('utf-8')))
